ed5fc4fea1d3fb0e7d4a59673e6854ff0df163cd
pawsingh <pawan.singh@digitaslbi.com>
2016-03-19 20:08:56 +0000

    initial commit

---
+++ PerfectSmile/PerfectSmile/Common/Log4NetLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Logging;
+
+namespace PerfectSmile.Common
+{
+    public class Log4NetLogger : ILoggerFacade
+    {
+        public void Log(string message, Category category, Priority priority)
+        {
+            switch (category)
+            {
+                case Category.Debug:
+                    //m_Logger.Debug(message);
+                    break;
+                case Category.Warn:
+                    //m_Logger.Warn(message);
+                    break;
+                case Category.Exception:
+                    //m_Logger.Error(message);
+                    break;
+                case Category.Info:
+                    //m_Logger.Info(message);
+                    break;
+            }
+
+        }
+    }
+}
