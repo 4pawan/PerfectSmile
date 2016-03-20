671482f5221f7c96eade0a9b229fe17153f1485a
pawsingh <pawan.singh@digitaslbi.com>
2016-03-20 18:47:36 +0000

    comitting latest changes

--- PerfectSmile/PerfectSmile/ViewModel/BaseViewModel.cs
+++ PerfectSmile/PerfectSmile/ViewModel/BaseViewModel.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PerfectSmile.Common;
 using Prism.Mvvm;
 
 namespace PerfectSmile.ViewModel
 {
-    public class BaseViewModel : BindableBase
+    public class BaseViewModel : ValidationBase
     {
 
     }
+
 }
