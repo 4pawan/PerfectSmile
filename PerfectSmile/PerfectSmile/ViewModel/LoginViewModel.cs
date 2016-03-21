219820077273189656d5273ed8437eda52ffe625
pawan.singh <pawan.singh@digitaslbi.com>
2016-03-21 20:50:31 +0530

    init commit

--- PerfectSmile/PerfectSmile/ViewModel/LoginViewModel.cs
+++ PerfectSmile/PerfectSmile/ViewModel/LoginViewModel.cs
@@ -3,8 +3,10 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 using PerfectSmile.Service;
+using PerfectSmile.View;
 
 namespace PerfectSmile.ViewModel
 {
@@ -24,17 +26,31 @@ namespace PerfectSmile.ViewModel
             set { SetProperty(ref _password, value); }
         }
 
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
         public ICommand LoginCommand { get; set; }
 
 
         public LoginViewModel()
         {
-            LoginCommand = new DelegateCommand(() =>
+            LoginCommand = new DelegateCommand<Window>((item) =>
             {
                 var isUserValid = LoginService.IsUserValid(Name, Password);
+                if (!isUserValid)
+                {
+                    Message = "Invalid User name or Password !";
+                }
+                else
+                {
+                    item.Close();
+                    new Shell().Show();
+                }
             });
-
-
         }
     }
 }
