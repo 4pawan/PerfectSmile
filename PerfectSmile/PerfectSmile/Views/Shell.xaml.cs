094679fcc1f3ceac0b7c2463fbed8d5973652524
pawan.singh <pawan.singh@digitaslbi.com>
2016-03-22 19:03:22 +0530

    init commit

--- PerfectSmile/PerfectSmile/Views/Shell.xaml.cs
+++ PerfectSmile/PerfectSmile/Views/Shell.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using Microsoft.Windows.Controls.Ribbon;
 
 namespace PerfectSmile.Views
 {
     /// <summary>
     /// Interaction logic for Shell.xaml
     /// </summary>
-    public partial class Shell : Window
+    public partial class Shell :RibbonWindow
     {
         public Shell()
         {
