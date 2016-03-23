5f2f8ff592d15df3832f053aacb27fe967e43d81
pawan.singh <pawan.singh@lbi.com>
2016-03-23 16:25:04 +0530

    init commit

---
+++ PerfectSmile/PerfectSmile.Test/Repository/LoginRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerfectSmile.Repository.Abstract;
+using PerfectSmile.Repository.Implementation;
+
+namespace PerfectSmile.Test.Repository
+{
+    [TestClass]
+    public class LoginRepositoryTest
+    {
+        private readonly ILoginRepository _loginRepository = new LoginRepository();
+
+
+        [TestMethod]
+        public void IsUserValidTest(string name, string password)
+        {
+            var asd = _loginRepository.IsUserValid("", "sdds");
+            Assert.IsFalse(asd);
+            Assert.IsFalse(_loginRepository.IsUserValid(null, null));
+            Assert.IsTrue(_loginRepository.IsUserValid("pk", "pk"));
+        }
+    }
+}
---
+++ PerfectSmile/PerfectSmile/Repository/Fakes/LoginRepositoryFake.cs
@@ -0,0 +1,13 @@
+﻿using PerfectSmile.Repository.Abstract;
+namespace PerfectSmile.Repository.Fakes
+{
+
+    public class LoginRepositoryFake : Implementation.Repository, ILoginRepository
+    {
+
+        public bool IsUserValid(string name, string password)
+        {
+            return true;
+        }
+    }
+}
