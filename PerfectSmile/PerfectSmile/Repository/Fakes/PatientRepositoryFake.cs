0e30f9dd96842647107acf16e5c1b0929e99ef9c
pawsingh <pawan.singh@digitaslbi.com>
2016-03-26 14:09:59 +0000

    init commit

---
+++ PerfectSmile/PerfectSmile/Repository/Fakes/PatientRepositoryFake.cs
@@ -0,0 +1,16 @@
+﻿using PerfectSmile.Repository.Abstract;
+using PerfectSmile.ViewModels;
+using PerfectSmile.Views;
+
+namespace PerfectSmile.Repository.Fakes
+{
+    public class PatientRepositoryFake : Implementation.Repository, IPatientRepository
+    {
+        public int AddPatientBasicInfo(PatientBasicFormViewModel vm)
+        {
+            var model = Helper.Helper.ConvertToPatientModel(vm);
+            TestPatientList.PatientList.Add(model);
+            return 1;
+        }
+    }
+}
---
+++ PerfectSmile/PerfectSmile/Repository/Fakes/TestPatientList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PerfectSmile.EF;
+
+namespace PerfectSmile.Repository.Fakes
+{
+    public static class TestPatientList
+    {
+        private static List<Patient> _patientList;
+
+        public static List<Patient> PatientList
+        {
+            get
+            {
+                if (_patientList == null)
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        _patientList.Add(new Patient
+                        {
+                            Name = "Patient" + i + 1,
+                            CreatedAt = DateTime.Now,
+                            CreatedBy = Environment.UserName,
+                            Remark = "121231232" + i,
+                        });
+                    }
+                }
+                return _patientList;
+            }
+            set { _patientList = value; }
+        }
+    }
+}
